594e6fd50c113779e1577c006e9db4ecf4db6410
AkshataSagare <akshatasagare17@gmail.com>
2025-06-10 01:23:59 +0530

    create textfield for amount, category and date and add date format

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,14 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AddExpense extends StatelessWidget {
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
 
   @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  void initState() {
+    dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Add Expenses',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+              SizedBox(height: 32,),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  controller: expenseController,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.currency_rupee, size: 20, 
+                     color: Colors.grey,
+                     ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none
+                    )
+                  ),
+                ),
+              ),
+              SizedBox(height: 16,),
+              TextFormField(
+                controller: categoryController,
+                textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.list, size: 20, 
+                     color: Colors.grey,
+                     ),
+                     hintText: 'Catagory',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none
+                    )
+                  ),
+              ),
+              SizedBox(height: 16,),
+              TextFormField(
+                controller: dateController,
+                textAlignVertical: TextAlignVertical.center,
+                readOnly: true,
+                onTap: () async{
+                  DateTime? newDate = await showDatePicker(
+                    context: context,
+                    initialDate: selectedDate, 
+                    firstDate: DateTime.now(), 
+                    lastDate: DateTime.now().add(Duration(days: 365))
+                  );
+
+                  if(newDate!= null) {
+                    setState(() {
+                      dateController.text = DateFormat('dd/MM/yyyy').format(newDate);
+                      selectedDate = newDate;
+                    });
+                  }
+
+
+                },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.date_range, size: 20, 
+                     color: Colors.grey,
+                     ),
+                     hintText: 'Date',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none
+                    )
+                  ),
+              ),
+              SizedBox(height: 32,),
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(12)
+                    )
+                  ), 
+                  child: Text(
+                    'Save',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                    ),
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
