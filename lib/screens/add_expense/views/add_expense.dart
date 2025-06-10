8ee540c8082a7e3a10d0c65c3a85778a4cfc0d32
AkshataSagare <akshatasagare17@gmail.com>
2025-06-11 00:01:55 +0530

    add category list

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -13,11 +13,86 @@ class _AddExpenseState extends State<AddExpense> {
   TextEditingController categoryController = TextEditingController();
   TextEditingController dateController = TextEditingController();
   DateTime selectedDate = DateTime.now();
+  IconData? selectedCategoryIcon;
+
+  final Map<String, List<Map<String, dynamic>>> categoryIcons = {
+    'Food': [
+      {'icon': Icons.food_bank, 'name': 'Food'},
+    ],
+    'Shopping': [
+      {'icon': Icons.local_grocery_store, 'name': 'Grocery'},
+      {'icon': Icons.checkroom, 'name': 'Clothing'},
+      {'icon': Icons.phone_android, 'name': 'Electronics'},
+      {'icon': Icons.book, 'name': 'Books'},
+      {'icon': Icons.edit, 'name': 'Stationary'},
+    ],
+    'Travel' : [
+      {'icon': Icons.mode_of_travel, 'name': 'Travel'},
+    ],
+    'Bills' : [
+      {'icon': Icons.smartphone, 'name': 'Phone Reacharge', },
+      {'icon': Icons.lightbulb, 'name': 'Light Bill'},
+      {'icon': Icons.gas_meter, 'name': 'Gas Bill'},
+    ],
+    'Health' : [
+      {'icon': Icons.health_and_safety, 'name': 'Health'},
+    ],
+    'Others' : [
+      {'icon': Icons.local_gas_station_sharp, 'name': 'Petrol'},
+      {'icon': Icons.movie, 'name': 'Movie'},
+      {'icon': Icons.currency_rupee, 'name': 'Other'},
+    ]
+  };
 
   @override
   void initState() {
     dateController.text = DateFormat('dd/MM/yyyy').format(DateTime.now());
     super.initState();
+  }
+
+  void showIconPicker(BuildContext context) {
+    final allItems = categoryIcons.entries.expand((e) => e.value).toList();
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          height: 400,
+          child: GridView.count(
+            crossAxisCount: 4,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            children: allItems.map((item) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedCategoryIcon = item['icon'];
+                    categoryController.text = item['name'];
+                  });
+                  Navigator.pop(context);
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.grey[200],
+                      child: Icon(item['icon'], color: Colors.blue),
+                    ),
+                    SizedBox(height: 5),
+                    Text(item['name'], textAlign: TextAlign.center,style: TextStyle(fontSize: 12)),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -36,12 +111,9 @@ class _AddExpenseState extends State<AddExpense> {
             children: [
               Text(
                 'Add Expenses',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500
-                ),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
               ),
-              SizedBox(height: 32,),
+              SizedBox(height: 32),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
                 child: TextFormField(
@@ -50,138 +122,64 @@ class _AddExpenseState extends State<AddExpense> {
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.currency_rupee, size: 20, 
-                     color: Colors.grey,
-                     ),
+                    prefixIcon: Icon(Icons.currency_rupee, size: 20, color: Colors.grey),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none
-                    )
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(height: 16),
               TextFormField(
                 controller: categoryController,
                 textAlignVertical: TextAlignVertical.center,
                 readOnly: true,
-                onTap: () {
-                  
-                },
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.list, size: 20, 
-                     color: Colors.grey,
-                     ),
-                     suffixIcon: IconButton(
-                      onPressed:() {
-                        showDialog(
-                          context: context, 
-                          builder: (ctx) {
-                            return AlertDialog(
-                              title: Text(
-                                'Create a Category'
-                              ),
-                              content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TextFormField(
-                                    //controller: dateController,
-                                    textAlignVertical: TextAlignVertical.center,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.white,
-                                      hintText: 'Name',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
-                                      )
-                                    ),
-                                  ),
-                                  SizedBox(height: 16,),
-                                  TextFormField(
-                                    //controller: dateController,
-                                    textAlignVertical: TextAlignVertical.center,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.white,
-                                      hintText: 'Icon',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
-                                      )
-                                    ),
-                                  ),
-                                  SizedBox(height: 16,),
-                                  TextFormField(
-                                    //controller: dateController,
-                                    textAlignVertical: TextAlignVertical.center,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.white,
-                                      hintText: 'Color',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                        borderSide: BorderSide.none
-                                      )
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-                        );
-                      } ,
-                      icon: Icon(
-                        Icons.add, 
-                        size: 22, 
-                        color: Colors.grey,
-                      ),
-                     ),
-                     hintText: 'Catagory',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none
-                    )
+                onTap: () => showIconPicker(context),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: selectedCategoryIcon != null
+                      ? Icon(selectedCategoryIcon, size: 20, color: Colors.grey)
+                      : Icon(Icons.list, size: 20, color: Colors.grey),
+                  hintText: 'Category',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
                   ),
+                ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(height: 16),
               TextFormField(
                 controller: dateController,
                 textAlignVertical: TextAlignVertical.center,
                 readOnly: true,
-                onTap: () async{
+                onTap: () async {
                   DateTime? newDate = await showDatePicker(
                     context: context,
-                    initialDate: selectedDate, 
-                    firstDate: DateTime.now(), 
-                    lastDate: DateTime.now().add(Duration(days: 365))
+                    initialDate: selectedDate,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime.now().add(Duration(days: 365)),
                   );
-
-                  if(newDate!= null) {
+                  if (newDate != null) {
                     setState(() {
                       dateController.text = DateFormat('dd/MM/yyyy').format(newDate);
                       selectedDate = newDate;
                     });
                   }
-
-
                 },
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.date_range, size: 20, 
-                     color: Colors.grey,
-                     ),
-                     hintText: 'Date',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none
-                    )
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.date_range, size: 20, color: Colors.grey),
+                  hintText: 'Date',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
                   ),
+                ),
               ),
-              SizedBox(height: 32,),
+              SizedBox(height: 32),
               SizedBox(
                 width: double.infinity,
                 height: kToolbarHeight,
@@ -190,16 +188,13 @@ class _AddExpenseState extends State<AddExpense> {
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.black,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(12)
-                    )
-                  ), 
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
                   child: Text(
                     'Save',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white
-                    ),
-                  )
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
                 ),
               )
             ],
