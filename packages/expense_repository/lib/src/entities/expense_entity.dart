835e09d3315dbabfc669bbafd4d04aabccc92385
AkshataSagare <akshatasagare17@gmail.com>
2025-06-29 22:24:08 +0530

    remove time from repository

--- packages/expense_repository/lib/src/entities/expense_entity.dart
+++ packages/expense_repository/lib/src/entities/expense_entity.dart
@@ -1,18 +1,15 @@
 import 'package:expense_repository/expense_repository.dart';
-import 'package:flutter/material.dart';
 
 class ExpenseEntity {
   String expenseId;
   Category category;
   DateTime date;
-  TimeOfDay time;
   int amount;
 
   ExpenseEntity({
     required this.expenseId,
     required this.category,
     required this.date,
-    required this.time,
     required this.amount,
 });
 
@@ -21,7 +18,6 @@ class ExpenseEntity {
       'expenseId': expenseId,
       'category': category.toEntity().toDocument(),
       'date': DateTime.now().toIso8601String(),
-      'time': TimeOfDay.now(),
       'amount': amount,
     };
   }
@@ -31,7 +27,7 @@ class ExpenseEntity {
       expenseId: doc['expenseId'],
       category: Category.fromEntity(CategoryEntity.fromDocument(doc['category'])),
       date:DateTime.parse(doc['date']).toLocal(),
-      time:,
+     
       amount: doc['amount'],
     );
   }
--- packages/expense_repository/lib/src/models/expense.dart
+++ packages/expense_repository/lib/src/models/expense.dart
@@ -1,18 +1,15 @@
 import 'package:expense_repository/expense_repository.dart';
-import 'package:flutter/material.dart';
 
 class Expense {
   String expenseId;
   Category category;
   DateTime date;
-  TimeOfDay time;
   int amount;
 
   Expense({
     required this.expenseId,
     required this.category,
     required this.date,
-    required this.time,
     required this.amount,
 });
 
@@ -20,7 +17,6 @@ class Expense {
     expenseId: '',
     category: Category.empty,
     date: DateTime.now(),
-    time: TimeOfDay.now(),
     amount: 0,
   );
 
@@ -29,7 +25,6 @@ class Expense {
       expenseId: expenseId,
       category: category,
       date: date,
-      time: time,
       amount:amount
     );
   }
@@ -39,7 +34,6 @@ class Expense {
       expenseId: entity.expenseId,
       category: entity.category,
       date: entity.date,
-      time: entity.time,
       amount: entity.amount,
     );
   }
